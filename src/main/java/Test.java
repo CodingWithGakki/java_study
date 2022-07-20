ba87a73e7b4b6bb737cbaccb21621241aaddb260
hide on bush <503465877@qq.com>
2022-07-20 09:34:18 +0800

    Update Test.java
    尝试rebase

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         System.out.println("this is from san jiang huayuan");
+        System.out.println("this is from online");
     }
 }

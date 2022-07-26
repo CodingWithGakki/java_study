920332737d8613570d0136ef4ed0fc787e02412b
wmq <503465877@qq.com>
2022-07-26 09:17:59 +0800

    更新readme123

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("idea 123");
         System.out.println("idea 1");
+        System.out.println("test");
     }
 }

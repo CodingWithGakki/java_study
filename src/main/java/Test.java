2c163d7d97c849644c453e57b921f9b9e64b3cbd
wmq <503465877@qq.com>
2023-01-30 15:11:17 +0800

    dev commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -11,6 +11,7 @@ public class Test {
     public static void main(String[] args) throws Exception {
         System.out.println("hello");
         System.out.println("hello2");
+        System.out.println("dev");
 
     }
 

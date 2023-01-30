7003f536c21c5ab588634a4c8539fb01133d61de
wmq <503465877@qq.com>
2023-01-30 15:12:16 +0800

    master commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -12,6 +12,7 @@ public class Test {
         System.out.println("hello");
         System.out.println("hello2");
         System.out.println("dev");
+        System.out.println("master");
 
     }
 
